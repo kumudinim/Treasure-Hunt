b1d395a97763212178282b106f3c959323fc3ff0
kumudinim <113531347+kumudinim@users.noreply.github.com>
2023-05-09 00:59:02 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,55 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "I am not alive,but I grow;I don't have lungs,but I need air;I don't have mouth,but I need water to live. What am I?\nHint:It's something ou can find in nature.",
+    answer: "Fire",
+  },
+    {
+    numb: 2,
+    question: "What has heart that does not beat?Hint:It's something you can hold in your hand.",
+    answer: "Artichoke",
+   
+  },
+  {
+    numb: 3,
+    question:"Listen to the audio clip and answer the given question.\n According to the speaker what is the latest area of law? <audio controls><source src='./laws.mp3' type='audio/mpeg'></audio>",
+    answer:"The intellectual property",
+    audio: "laws.mp3",
+    
+   
+  },
+ {
+  numb:4,
+  question: "If it takes 18 machines 18 minutes to make 18 widgets, how long would it take 100 machines to make 100 widgets? Hint:Jersey number of Indian cricketer and is multiple of 3 and 6.",
+  answer: "18",
+
+ },
+ {
+  numb: 5,
+  question: "Listen to the audio clip and answer the given question.\n What is one of the softwares required to keep computers safe and secure? <audio controls><source src='./hacking.mp3' type='audio/mpeg'></audio>",
+  answer: "firewalls",
+  audio: "hacking.mp3",
+},
+{
+  numb:6,
+  question: "1, 10, 9, x, 25, 26, 49. Find x.",
+  answer:"17",
+},
+
+{
+  numb: 7,
+  question: "Which container will be filled first ? <img src='brain teaser.png'>",
+  answer: "F",
+},
+{
+  numb: 8,
+  question: "Write your answer. <img src='Math.jpg'>",
+  answer: "569",
+},
+
+
+
+]
+
+
